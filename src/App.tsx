2d09d059c0943d74df6031d5e3f1f0ed9de98964
Gabriel <biel_borel@hotmail.com>
2022-08-22 23:20:16 -0300

    Added Theme-provider with default-theme to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import { Router } from "./Router";
 import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Router />
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyle />
-    </BrowserRouter>
+    </ThemeProvider>
   );
 };
