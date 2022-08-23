14b4782f6f08923327b38f6b659be1c073cb9f6d
Gabriel <biel_borel@hotmail.com>
2022-08-23 13:04:47 -0300

    CoffeeList component created

---
+++ src/pages/Home/components/CoffeeList/index.tsx
@@ -0,0 +1,11 @@
+import { CoffeeListContainer } from './styles';
+
+export const CoffeeList = () => {
+  return (
+    <CoffeeListContainer>
+      <strong>Nossos cafés</strong>
+
+      <div>asdhasdh</div>
+    </CoffeeListContainer>
+  );
+};
---
+++ src/pages/Home/components/CoffeeList/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const CoffeeListContainer = styled.section`
+  strong {
+    color: ${(props) => props.theme['base-title']};
+    font-family: 'Baloo 2', cursive;
+    font-size: 2rem;
+  }
+`;
