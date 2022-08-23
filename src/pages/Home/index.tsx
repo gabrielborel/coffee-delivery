9a2cb793301a8153ef4c44f83d9511d0f44c0ef7
Gabriel <biel_borel@hotmail.com>
2022-08-23 13:05:06 -0300

    Added CoffeeList component to the Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,15 @@
 import { HomeContainer } from './styles';
 import { HeroBanner } from './components/HeroBanner';
+import { BlurBackground } from './components/HeroBanner/styles';
+import { CoffeeList } from './components/CoffeeList';
 
 export const Home = () => {
   return (
     <HomeContainer>
+      <BlurBackground />
       <HeroBanner />
+
+      <CoffeeList />
     </HomeContainer>
   );
 };
