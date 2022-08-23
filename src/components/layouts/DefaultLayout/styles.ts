b1d366a3d41ff6e0454690aaac6293368a3cb46c
Gabriel <biel_borel@hotmail.com>
2022-08-23 11:06:58 -0300

    Some css changes on default layout

--- src/components/layouts/DefaultLayout/styles.ts
+++ src/components/layouts/DefaultLayout/styles.ts
@@ -1,10 +1,9 @@
 import styled from 'styled-components';
 
-export const LayoutContainer = styled.main`
-  max-width: 70rem;
-  margin: 0 auto;
+export const LayoutContainer = styled.div`
+  width: 100%;
+  max-width: 74rem;
+  height: calc(100vh - 4rem);
   padding: 2rem;
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
+  margin: 0 auto;
 `;
