6d300b49953d7982aa69d7eaf371897548f0a767
Gabriel <biel_borel@hotmail.com>
2022-08-23 01:20:34 -0300

    Default layout for all pages created

---
+++ src/components/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const LayoutContainer = styled.main`
+  max-width: 70rem;
+  margin: 0 auto;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
