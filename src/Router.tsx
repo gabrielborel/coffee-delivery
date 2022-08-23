e3afed89a4d46fc966d2234c11cf62aca38fabac
Gabriel <biel_borel@hotmail.com>
2022-08-23 01:21:09 -0300

    Added default layout to all routes on router component

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
+import { DefaultLayout } from './components/layouts/DefaultLayout';
 import { Home } from './pages/Home';
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<DefaultLayout />}>
+        <Route path='/' element={<Home />} />
+      </Route>
     </Routes>
   );
 };
