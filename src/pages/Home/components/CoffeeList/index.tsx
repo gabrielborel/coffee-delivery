d83307c4a5a62a16f9d188b9e089d20fb7997271
Gabriel <biel_borel@hotmail.com>
2022-08-24 14:29:05 -0300

    Moved CoffeeCard JSX and CSS to its own file

--- src/pages/Home/components/CoffeeList/index.tsx
+++ src/pages/Home/components/CoffeeList/index.tsx
@@ -1,6 +1,6 @@
-import { CoffeeCard, CoffeeListContainer, CoffeesContainer } from './styles';
+import { CoffeeListContainer, CoffeesContainer } from './styles';
 import { CoffeesList } from './data';
-import { ShoppingCart, Plus, Minus } from 'phosphor-react';
+import { CoffeeCard } from './CoffeeCard';
 
 export const CoffeeList = () => {
   return (
@@ -9,40 +9,7 @@ export const CoffeeList = () => {
 
       <CoffeeListContainer>
         {CoffeesList.map((coffee) => (
-          <CoffeeCard key={coffee.id}>
-            <header>
-              <img src={coffee.image} alt='' />
-
-              <div>
-                {coffee.labels.map((label) => (
-                  <span>{label}</span>
-                ))}
-              </div>
-            </header>
-
-            <main>
-              <strong>{coffee.name}</strong>
-              <p>{coffee.description}</p>
-            </main>
-
-            <footer>
-              <span>
-                R$ <strong>{coffee.price.toFixed(2)}</strong>
-              </span>
-
-              <div>
-                <div>
-                  <Minus weight='bold' size={20} />
-                  <input type='number' defaultValue={1} />
-                  <Plus weight='bold' size={20} />
-                </div>
-
-                <button>
-                  <ShoppingCart size={20} weight='fill' />
-                </button>
-              </div>
-            </footer>
-          </CoffeeCard>
+          <CoffeeCard coffee={coffee} key={coffee.id} />
         ))}
       </CoffeeListContainer>
     </CoffeesContainer>
