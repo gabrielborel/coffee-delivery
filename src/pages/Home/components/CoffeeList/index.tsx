904fb7e4b0ffeaf04d2e5f7fabd7315cffc85423
Gabriel <biel_borel@hotmail.com>
2022-08-24 14:16:26 -0300

    Created CoffeeList and CoffeeCard component

--- src/pages/Home/components/CoffeeList/index.tsx
+++ src/pages/Home/components/CoffeeList/index.tsx
@@ -1,11 +1,50 @@
-import { CoffeeListContainer } from './styles';
+import { CoffeeCard, CoffeeListContainer, CoffeesContainer } from './styles';
+import { CoffeesList } from './data';
+import { ShoppingCart, Plus, Minus } from 'phosphor-react';
 
 export const CoffeeList = () => {
   return (
-    <CoffeeListContainer>
+    <CoffeesContainer>
       <strong>Nossos cafés</strong>
 
-      <div>asdhasdh</div>
-    </CoffeeListContainer>
+      <CoffeeListContainer>
+        {CoffeesList.map((coffee) => (
+          <CoffeeCard key={coffee.id}>
+            <header>
+              <img src={coffee.image} alt='' />
+
+              <div>
+                {coffee.labels.map((label) => (
+                  <span>{label}</span>
+                ))}
+              </div>
+            </header>
+
+            <main>
+              <strong>{coffee.name}</strong>
+              <p>{coffee.description}</p>
+            </main>
+
+            <footer>
+              <span>
+                R$ <strong>{coffee.price.toFixed(2)}</strong>
+              </span>
+
+              <div>
+                <div>
+                  <Minus weight='bold' size={20} />
+                  <input type='number' defaultValue={1} />
+                  <Plus weight='bold' size={20} />
+                </div>
+
+                <button>
+                  <ShoppingCart size={20} weight='fill' />
+                </button>
+              </div>
+            </footer>
+          </CoffeeCard>
+        ))}
+      </CoffeeListContainer>
+    </CoffeesContainer>
   );
 };
--- src/pages/Home/components/CoffeeList/styles.ts
+++ src/pages/Home/components/CoffeeList/styles.ts
@@ -1,9 +1,147 @@
 import styled from 'styled-components';
 
-export const CoffeeListContainer = styled.section`
+export const CoffeesContainer = styled.section`
   strong {
     color: ${(props) => props.theme['base-title']};
     font-family: 'Baloo 2', cursive;
     font-size: 2rem;
+  }
+`;
+
+export const CoffeeListContainer = styled.div`
+  margin-top: 3rem;
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(4, 1fr);
+`;
+
+export const CoffeeCard = styled.div`
+  width: 16rem;
+  height: 19rem;
+  background: ${(props) => props.theme['base-card']};
+  border-radius: 6px 36px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  padding: 0 1.25rem;
+
+  header,
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  header {
+    img {
+      margin-top: -1rem;
+    }
+
+    div {
+      display: flex;
+      gap: 0.25rem;
+
+      span {
+        background: ${(props) => props.theme['yellow-light']};
+        color: ${(props) => props.theme['yellow-dark']};
+        padding: 0.25rem 0.5rem;
+        border-radius: 100px;
+        text-transform: uppercase;
+        font-weight: 700;
+        font-size: 0.625rem;
+        line-height: 1.3;
+      }
+    }
+  }
+
+  main {
+    strong {
+      font-size: 1.25rem;
+      font-weight: 700;
+      line-height: 1.6;
+      color: ${(props) => props.theme['base-subtitle']};
+    }
+
+    p {
+      text-align: center;
+      font-size: 0.8rem;
+      line-height: 1.3;
+      color: ${(props) => props.theme['base-label']};
+    }
+  }
+
+  footer {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 1.4375rem;
+
+    span {
+      color: ${(props) => props.theme['base-text']};
+      font-size: 0.8rem;
+
+      strong {
+        color: ${(props) => props.theme['base-text']};
+        font-size: 1.75rem;
+      }
+    }
+
+    div {
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+
+      > div {
+        display: flex;
+        align-items: center;
+        gap: 0.25rem;
+        border-radius: 6px;
+        padding: 0.5rem;
+        width: 4rem;
+        background: ${(props) => props.theme['base-button']};
+
+        svg {
+          color: ${(props) => props.theme.purple};
+          cursor: pointer;
+          transition: color 0.1s;
+
+          &:hover {
+            color: ${(props) => props.theme['purple-dark']};
+          }
+        }
+
+        input {
+          width: 1.25rem;
+          background: transparent;
+          border: 0;
+          text-align: center;
+          -webkit-appearance: textfield;
+          -moz-appearance: textfield;
+          appearance: textfield;
+
+          &::-webkit-inner-spin-button,
+          &::-webkit-outer-spin-button {
+            -webkit-appearance: none;
+          }
+        }
+      }
+
+      button {
+        border: 0;
+        border-radius: 6px;
+        line-height: 0;
+        padding: 0.5rem;
+        background: ${(props) => props.theme['purple-dark']};
+        color: ${(props) => props.theme.white};
+        cursor: pointer;
+        transition: background-color 0.1s;
+
+        &:hover {
+          background-color: ${(props) => props.theme.purple};
+        }
+      }
+    }
   }
 `;
